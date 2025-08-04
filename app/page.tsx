edf7f8d114d271941ccdc73041afaf21e527d7c2
Jiya <jiyasahni14@gmail.com>
2025-08-04 20:06:47 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from '@/components/ui/button';
 import React from 'react';
 
 const Page = () => {
   return (
     <div>
       <h1>Home</h1>
+      <br/>
+      <Button>HELLO</Button>
     </div>
   );
 };
